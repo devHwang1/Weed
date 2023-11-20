3397a578d33e3c53581a1a8d161b4f806491d542
devHwang1 <comeday7741@gmail.com>
2023-11-20 14:54:13 +0900

    weed Project Start

---
+++ src/main/java/com/example/weed/WeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.weed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weed/WeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
