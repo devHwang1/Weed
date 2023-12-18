735b2518bfb81d64a67ed757807c1f9582c64423
SeungjaeDanLee <vqs210@gmail.com>
2023-12-18 15:43:05 +0900

    일단 삭제

--- src/main/java/com/example/weed/domain/members/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.weed.domain.members;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Member findByEmail(String email);
-}
